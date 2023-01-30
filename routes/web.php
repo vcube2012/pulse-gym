5c0d49a1b7839c70254983d3533314180cada788
tsar <vcube.su@gmail.com>
2023-01-30 15:56:18 +0200

    init

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('test', function () {
     app(\App\Service\TranslateService::class)->make();
 });
+Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localize']], function () {
     Route::any('/{view?}', function () {
         return view('welcome');
     })->where('view', '^(?!nova|nova-api|nova-vendor).*$');
+});

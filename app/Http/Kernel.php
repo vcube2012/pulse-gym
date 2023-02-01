6b65a082ecee7f4ebd9dd2c30d55eab8159fbade
tsar <vcube.su@gmail.com>
2023-02-01 11:30:49 +0200

    init

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Localization;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -65,6 +66,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'localization' => Localization::class,
         'localize'                => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
         'localizationRedirect'    => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
         'localeSessionRedirect'   => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware("localization")->group(function () {
     Route::get('/tags', [\App\Http\Controllers\TagController::class, 'index']);
     Route::get('blogs', [\App\Http\Controllers\BlogController::class, 'index']);
     Route::get('blogs/{blog:slug}', [\App\Http\Controllers\BlogController::class, 'show']);
@@ -25,3 +27,4 @@ use Illuminate\Support\Facades\Route;
     Route::get('page/{page:slug}', [\App\Http\Controllers\PageController::class, 'show']);
     Route::get('promo', [\App\Http\Controllers\PromoController::class, 'index']);
 
+});

63f9ea1b8a7c542ce923847cf1cf8e60b9a2937f
Nazik4114 <nazikban14@gmail.com>
2023-02-02 14:37:57 +0200

    feedback control

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('test', function () {
 //    app(\App\Service\TranslateService::class)->make();
-    Artisan::command('migrate:fresh --seed --force',function (){
-        return 0;
-    });
+   return Artisan::call('migrate:fresh --seed --force');
 });
 Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localize']], function () {
     Route::any('/{view?}', function () {

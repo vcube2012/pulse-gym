9608a46b478c24795bf13329e7bc421536ce0d78
tsar <vcube.su@gmail.com>
2023-02-03 20:29:09 +0200

    init

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -5,14 +5,16 @@ namespace App\Http\Middleware;
 use App;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 class Localization
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse) $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
@@ -21,7 +23,10 @@ class Localization
             /**
              * If Accept-Language header found then set it to the default locale
              */
-            App::setLocale($request->header("Accept-Language"));
+            $lang = explode(',', $request->header("Accept-Language"))[0];
+            if (in_array($lang, array_keys(LaravelLocalization::getSupportedLocales()))) {
+                App::setLocale($lang);
+            }
         }
         return $next($request);
     }

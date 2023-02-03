962f481cc066ffb9290fe1b11692f02e0b962bc5
tsar <vcube.su@gmail.com>
2023-02-03 13:09:09 +0200

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,7 +92,7 @@
                 opacity: .2
             }
         }</style>
-    <script defer="defer" src="/static/js/main.5b34dea4.js"></script>
+    <script defer="defer" src="/static/js/main.36e8f8af.js"></script>
     <link href="/static/css/main.de5316b3.css" rel="stylesheet">
 </head>
 <body>

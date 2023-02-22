46bddecbb8061c03ca322b267c260a993beb7aed
Nazik4114 <nazikban14@gmail.com>
2023-02-22 10:20:30 +0200

    add baner

--- app/Http/Resources/PriceCategoryResource.php
+++ app/Http/Resources/PriceCategoryResource.php
@@ -21,7 +21,7 @@ class PriceCategoryResource extends JsonResource
             'new' => $this->new,
             'actia' => $this->actia,
             'comment' => $this->comment,
-            'price' => PriceResource::collection($this->order()->get()),
+            'price' => PriceResource::collection($this->order()),
             'baner'=>BanerResource::collection($this->whenLoaded('baners')),
         ];
     }

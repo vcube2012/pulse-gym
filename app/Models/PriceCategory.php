214e16c4f3fddfdf161f5b6981ced30b1d1c3e88
Nazik4114 <nazikban14@gmail.com>
2023-02-24 14:50:24 +0200

    front

--- app/Models/PriceCategory.php
+++ app/Models/PriceCategory.php
@@ -59,15 +59,29 @@ class PriceCategory extends Model
 
     public function scopeOrder()
     {
-        if($this->price()->get()->count()!=0) {
-            $price = $this->price()->orderBy('price', 'asc')->get();
-
-            if (Str::contains($price->first()->name, ['Единоразовый абонемент', 'Одноразовий абонемент', 'Разове тренування', 'Разовая тренировка'])) {
+        $price = $this->price()->orderBy('price', 'asc')->get();
+        if($price->isNotEmpty()) {
+            if (in_array($price->first()->name, ['Единоразовый абонемент', 'Одноразовий абонемент', 'Разове тренування', 'Разовая тренировка'])) {
                 $singl = $price->first();
                 $price->push($singl);
                 $price->forget(0);
+                $price->values();
             }
+            if($price->count()>2) {
+                if (in_array($price[1]->name, ['Единоразовый абонемент', 'Одноразовий абонемент', 'Разове тренування', 'Разовая тренировка'])) {
+                    $singl = $price[1];
+                    $price->push($singl);
+                    $price->forget(1);
+//                    dd($price);
 
+                }
+            }
+            if (in_array($price->first()->name, ['Призупинення абонементу (2 тижні)', 'Приостановка абонемента (2 недели)'])) {
+                $singl = $price->first();
+                $price->push($singl);
+                $price->forget(0);
+                $price->values();
+            }
             return $price;
         }
         else{

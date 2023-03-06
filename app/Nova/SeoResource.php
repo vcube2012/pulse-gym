7d2d111befc4d587b59f10d95dc5b308650ccb90
Nazik4114 <nazikban14@gmail.com>
2023-03-06 11:50:43 +0200

    front

--- app/Nova/SeoResource.php
+++ app/Nova/SeoResource.php
@@ -22,7 +22,7 @@ class SeoResource extends Resource
      *
      * @var string
      */
-    public static $title = 'id';
+    public static $title = 'name';
 
     /**
      * The columns that should be searched.
@@ -33,7 +33,7 @@ class SeoResource extends Resource
         'id',
     ];
 
-    public static $displayInNavigation = false;
+//    public static $displayInNavigation = false;
 
     /**
      * Get the fields displayed by the resource.

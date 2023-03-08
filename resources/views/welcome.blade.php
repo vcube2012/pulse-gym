85df12c66c113b3721d656bd9535026f8830d3de
Nazik4114 <nazikban14@gmail.com>
2023-03-08 12:23:26 +0200

    front

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,7 +96,7 @@
                 opacity: .2
             }
         }</style>
-    <script defer="defer" src="{{asset('/static/js/main.905141af.js')}}"></script>
+    <script defer="defer" src="{{asset('/static/js/main.0b33bef7.js')}}"></script>
     <link href="{{asset('/static/css/main.ea679065.css')}}" rel="stylesheet">
 </head>
 <body>

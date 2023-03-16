dc1480626a29d3e86b38f749a518054bd6fb62b3
Nazik4114 <nazikban14@gmail.com>
2023-03-16 12:26:48 +0200

    mail sender

--- app/Service/SendMail.php
+++ app/Service/SendMail.php
@@ -24,15 +24,15 @@ class SendMail
 
     public function setConfig()
     {
+        config()->set('mail.mailers.smtp.transport', $this->config->driver);
         config()->set('mail.mailers.smtp.host', $this->config->host);
         config()->set('mail.mailers.smtp.port', $this->config->port);
         config()->set('mail.mailers.smtp.username', $this->config->username);
         config()->set('mail.mailers.smtp.password', $this->config->password);
         config()->set('services.mailgun.domain', $this->config->domain);
         config()->set('services.mailgun.secret', $this->config->secret);
-        config()->set('mail.from.address', $this->config->address);
-        config()->set('mail.from.name', $this->config->name);
-        config()->set('mail.from.name', $this->config->to);
+        config()->set('mail.from.address', $this->config->from_address);
+        config()->set('mail.from.name', $this->config->from_name);
         return $this;
     }
 }
